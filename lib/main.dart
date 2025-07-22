e7719f45303258e45e5b6ac76e293533aa198410
F.Ege Eryigit <f.ege.eryigit@gmail.com>
2025-07-22 11:18:22 +0300

    First section completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,152 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  double availableScreenWidth = 0;
+
+  @override
   Widget build(BuildContext context) {
+    availableScreenWidth = MediaQuery.of(context).size.width - 50;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.red),
-        body: Center(child: Text('FEGE')),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+              alignment: Alignment.bottomCenter,
+              height: 170,
+              decoration: BoxDecoration(color: Colors.blue.shade700),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'File Storage',
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'by fege',
+                        style: TextStyle(fontSize: 17, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(.1),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.search,
+                            size: 28,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(.1),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.notifications_outlined,
+                            size: 28,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      text: "Storage ",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: "9.1/10GB",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    "Upgrade",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue.shade700,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                children: [
+                  buildFileSizeChart('SOURCES', Colors.blue, .3),
+                  buildFileSizeChart('DOCS', Colors.red, .25),
+                  buildFileSizeChart('IMAGES', Colors.orangeAccent, .20),
+                  buildFileSizeChart('', Colors.grey, .23),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Column buildFileSizeChart(String title, Color color, double width) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(width: availableScreenWidth * width, height: 4, color: color),
+        SizedBox(height: 8),
+        Text(
+          title,
+          style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 }
